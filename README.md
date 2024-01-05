8960e843788da0c9342dea31d04fe38e20e4a569
Your Name <you@example.com>
2024-01-05 06:11:25 +0000

    try

---
+++ README.md
@@ -0,0 +1,30 @@
+# TonLink Npm
+
+This repo is to build a browser base light wallet for TON blockchain
+
+It will allows anyone to send some `Cash Gift` by TON blockchain
+
+## Feature
+
+- Use `tonweb`` to make pair generate in EVM chain .
+
+- Add TON txns to make  simple transactions .
+
+- Generate new link (which allows to be paid) . 
+
+# What is TonLink?
+
+TonLink is a lightweight wallet designed to make TON blockchian assets transfer as easy as sending a link. It allows any web3auth/unipass/magicWallet user to easly transfer money and share any kinds of token including NFT easly by linkes . 
+
+# Basic Installation instructions
+```bash
+npm install @tonlink/api
+```
+Import Instructions
+```js
+import { TonLink } from '@tonlink';
+```
+Create a TonLink
+```js
+
+```
---
+++ src/index.ts
@@ -0,0 +1,177 @@
+import * as TonWeb from "tonweb";
+
+import * as tonc from "ton-crypto";
+
+import * as b58 from "b58";
+
+import * as orbs from "@orbs-network/ton-access";
+
+import * as ton from "ton"
+
+const nacl = TonWeb.nacl;
+
+/**
+ * 
+ * @returns string
+ * Random a new ton account and encode it into b58
+ */
+
+const newSecretKey = () =>
+{
+  const keyPair = nacl.sign.keyPair();
+  return b58.encode(keyPair.secretKey);
+}
+
+/**
+ * 
+ * @param secretKey 
+ * @returns uint8Array
+ * 
+ * Decode base58 sec into uint8Arr
+ */
+const seedToKeypair= (secretKey : string) =>
+{
+    return tonc.keyPairFromSecretKey(
+        b58.decode(
+            secretKey
+        )
+    );
+}
+/**
+ * 
+ * @param data 
+ * @returns {} OR false
+ * 
+ * To decode the base58 raw-data into readable json
+ * 
+ * The rules of Url :
+ * {baseurl}/{Data}
+ */
+const decodeLink = (data : string) =>
+{
+  try{
+    const raw = JSON.parse(
+      b58.decode(
+        data
+      )
+    )
+    return raw
+  }catch(e)
+  {
+    return false;
+  }
+}
+
+const encodeLink = (baseUrl : string,s : string,c:string) =>
+{
+  return baseUrl + b58.encode(
+    Buffer.from(
+        JSON.stringify(
+            {
+                t : 0,
+                s : s,
+                c : c
+            }
+        )
+    )
+) 
+}
+
+const signTxn = async (target : string,amount : number , t : TonLink) =>
+{
+    const key = t.keypair
+    const keyData = TonLink.getTonKeyPair(t)
+    
+    const endpoint = await orbs.getHttpEndpoint({ network: t.network });
+    const client = new ton.TonClient({ endpoint });
+    let walletContract = client.open(keyData);
+    let seqno = await walletContract.getSeqno();
+
+    var txs = {
+        secretKey: key.secretKey,
+        seqno: seqno,
+        messages: [
+        ]
+      }
+    txs.messages.push(
+        ton.internal(
+        {
+          to: target, 
+          value: amount,
+          bounce: false,
+        }
+      )
+    )
+    await walletContract.sendTransfer(txs);
+
+}
+
+
+
+ /**
+  * TONLINK Class 
+  * 
+  * - Create new wallet in link
+  * - Recover wallet by link
+  */
+export class TonLink {
+  baseurl: string;
+  keypair: any;
+  link:string;
+  workchain:number;
+  network:string;
+  addressType:string;
+  //Init this object 
+  private constructor(baseurl: string, s: string ,link:string, workchain:number, network:string, addressType:string) {
+    this.baseurl = (baseurl=="")?"http://localhost/":baseurl;
+    this.keypair = (s=="")?newSecretKey():seedToKeypair(s);
+    this.link = (link=="")?encodeLink(this.baseurl,this.keypair.secretKey,""):link;
+    this.workchain = workchain;
+    this.network = (network=="")?"mainnet":network;
+    this.addressType = (addressType == "")?"WalletContractV3R2":addressType;
+  }
+
+  /**
+   * 
+   * @param raw : The data that transfer
+   * @returns 
+   * 
+   */
+  public static fromRawData(raw:string)
+  {
+    const data = decodeLink(raw);
+    if(data)
+    {
+      return new TonLink(
+        "",
+        data.s,
+        "",
+        1,
+        "testnet",
+        ""
+      )
+    }
+    return false;
+  }
+
+  public static getTonKeyPair(t:TonLink){
+    switch(t.addressType)
+    {
+      case "WalletContractV1R1":
+        return ton.WalletContractV1R1.create({ publicKey: t.keypair.publicKey, workchain: t.workchain });
+      case "WalletContractV1R2":
+        return ton.WalletContractV1R2.create({ publicKey: t.keypair.publicKey, workchain: t.workchain });
+      case "WalletContractV2R1":
+        return ton.WalletContractV2R1.create({ publicKey: t.keypair.publicKey, workchain: t.workchain });
+      case "WalletContractV2R2":
+        return ton.WalletContractV2R2.create({ publicKey: t.keypair.publicKey, workchain: t.workchain });
+      case "WalletContractV3R1":
+        return ton.WalletContractV3R1.create({ publicKey: t.keypair.publicKey, workchain: t.workchain });
+      case "WalletContractV3R2":
+        return ton.WalletContractV3R2.create({ publicKey: t.keypair.publicKey, workchain: t.workchain });
+      default :
+        break;
+    }
+  }
+
+}
